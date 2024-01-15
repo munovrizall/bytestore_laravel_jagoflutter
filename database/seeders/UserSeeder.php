80fe957ea408f2f40f58806185d4bd30ffc16c9f
Mohammad Novrizal <muhammadnovrizal87@gmail.com>
2024-01-15 17:07:17 +0700

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory(10)->create();
+
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Admin Rijal',
+            'email' => 'rijal@email.com',
+            'password' => Hash::make('admin'),
+        ]);
+    }
+}
