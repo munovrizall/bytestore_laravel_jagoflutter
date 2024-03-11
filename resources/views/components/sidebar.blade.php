6dc09a5832be6ccc0ea298f416391c7f4acd22b9
Mohammad Novrizal <muhammadnovrizal87@gmail.com>
2024-03-11 22:12:48 +0700

    name updated in sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">Online Shop CWB</a>
+            <a href="index.html">Byte Store</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
             <a href="index.html">St</a>
